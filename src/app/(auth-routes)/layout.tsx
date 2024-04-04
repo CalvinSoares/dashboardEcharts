8c62a82488eca9252ac9b35eda3e554b63c6127b
Calvin <calvinsoares17@gmail.com>
2024-04-04 13:12:12 -0300

    bugfix: erro na hora do build e deploy solucionado

--- src/app/(auth-routes)/layout.tsx
+++ src/app/(auth-routes)/layout.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
-import { nextAuthOptions } from "../api/auth/[...nextauth]/route";
+import { nextAuthOptions } from "../utils/authOptions";
 import { redirect } from "next/navigation";
 
 interface PrivateLayoutProps {
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,4 @@ import NextAuth from "next-auth";
 
 const handler = NextAuth(nextAuthOptions);
 
-export { handler as GET, handler as POST, nextAuthOptions };
+export { handler as GET, handler as POST };
