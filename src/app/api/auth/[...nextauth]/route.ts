7aeb4e1cb328bff07f53fab9ebca5da4763b8dd9
Calvin <calvinsoares17@gmail.com>
2024-04-04 00:17:58 -0300

    hotfix: erro no build, nextAuthOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,45 +1,5 @@
-import axios from "axios";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const nextAuthOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "email", type: "email" },
-        password: { label: "password", type: "password" },
-      },
-
-      async authorize(credentials, req) {
-        try {
-          const response = await axios.post(
-            "http://localhost:3000/auth/login",
-            {
-              email: credentials?.email,
-              password: credentials?.password,
-            }
-          );
-
-          const user = response.data;
-
-          if (user) {
-            return user;
-          }
-
-          return null;
-        } catch (error) {
-          console.error("Error during login:", error);
-          return null;
-        }
-      },
-    }),
-  ],
-
-  pages: {
-    signIn: "/",
-  },
-};
+import { nextAuthOptions } from "@/app/utils/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(nextAuthOptions);
 
---
+++ src/app/utils/authOptions.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const nextAuthOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        try {
+          const response = await axios.post(
+            "http://localhost:3000/auth/login",
+            {
+              email: credentials?.email,
+              password: credentials?.password,
+            }
+          );
+
+          const user = response.data;
+
+          if (user) {
+            return user;
+          }
+
+          return null;
+        } catch (error) {
+          console.error("Error during login:", error);
+          return null;
+        }
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: "/",
+  },
+};
