495043ccd86faaa6beef3f62d5c7e94d19acfe03
Calvin <calvinsoares17@gmail.com>
2024-04-04 13:52:43 -0300

    bugfix: erro na hora do build e deploy solucionado

--- src/app/(admin-routes)/layout.tsx
+++ src/app/(admin-routes)/layout.tsx
@@ -1,3 +1,5 @@
+
+
 import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
 import { nextAuthOptions } from "../utils/authOptions";
--- src/app/(auth-routes)/page.tsx
+++ src/app/(auth-routes)/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
