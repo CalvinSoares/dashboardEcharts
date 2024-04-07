ef267c7b3e8dcb3c5f7aa007ebdc9dd47e12fad9
Calvin <calvinsoares17@gmail.com>
2024-04-07 17:55:46 -0300

    feat: metodo callback para retornar o token

--- src/app/utils/authOptions.ts
+++ src/app/utils/authOptions.ts
@@ -1,6 +1,24 @@
 import axios from "axios";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { JWT } from "next-auth/jwt";
+import { User } from "next-auth";
+
+type TypeJWT = {
+  sub: string;
+  id: number;
+  email: string;
+  token: string;
+  iat: number;
+  exp: number;
+  jti: string;
+};
+
+type TypeUser = {
+  id: number;
+  email: string;
+  access_token: string;
+};
 
 export const nextAuthOptions: NextAuthOptions = {
   providers: [
@@ -35,8 +53,45 @@ export const nextAuthOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    jwt: async ({
+      token,
+      user,
+    }: {
+      token: JWT;
+      user: User;
+    }): Promise<TypeJWT | JWT> => {
+      if (user) {
+        token.id = user.id;
+      }
+      if (user) {
+        const data: TypeUser = user as unknown as any;
+        return {
+          ...token,
+          id: data.id,
+          email: data.email,
+          token: data.access_token,
+        } as TypeJWT;
+      }
+      return token as JWT;
+    },
+    session: async ({ session, token }: any) => {
+      if (token) {
+        return {
+          ...session,
+          user: {
+            token: token.token,
+          },
+        };
+      }
+      return session;
+    },
+  },
   pages: {
     signIn: "/",
+  },
+  session: {
+    strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
